df7c17073bc21dc053a356f4dc4914169f19e7d3
Joe Cyboski <cybo42@gmail.com>
2014-06-05 14:00:24 -0400

    added promises to close out connection properly

--- fsfun.js
+++ fsfun.js
@@ -3,8 +3,11 @@ var name = process.argv[2];
 var mime = require('mime');
 
 var mongo = require('mongodb'), 
+
 MongoClient = mongo.MongoClient,
 Grid = mongo.Grid;
+
+var Q = require('q');
 
 console.log("Reading file " + name);
 MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
@@ -16,31 +19,44 @@ MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
 		}
 		var zip = new require('node-zip')(data, {base64: false, checkCRC32: true});
 
-		console.log(typeof zip.files);
-		Object.keys(zip.files).forEach(function(name){
+		console.dir(Object.keys(zip.files));
+		var promises = Object.keys(zip.files).map(function(name){
+			var d = Q.defer();
 			var entry = zip.files[name];
-	     		console.dir(entry);
-	     		if(!entry.options.dir){
-	     			var grid = new Grid(db, 'fs');
-	     			var contentType = mime.lookup(name);
-	     			console.log("Mime %s", contentType);
-	     			var buffer = (entry.options.binary)? entry.asNodeBuffer(): new Buffer(entry._data);
-	     			grid.put(buffer, {
-	     				_id: {bundle: "cn-fe-ads", ver: "1.0.0", path: name}, 
-	     				metadata:{category:'text'},
-	     				content_type: contentType }, function(err, fileInfo) {
-	     					if(err){ 
-	     						console.log("hello");
-	     						throw err;
-	     					}
-	     					console.dir(fileInfo);
-		     		});
-	     		}
-	     	});
+			console.dir(entry);
 
-			// db.close();
-	     });
+			if(!entry.options.dir){
+				var grid = new Grid(db, 'fs');
+				var contentType = mime.lookup(name);
+				console.log("Mime %s", contentType);
+				var buffer = (entry.options.binary)? entry.asNodeBuffer(): new Buffer(entry._data);
+				grid.put(buffer, 
+					{_id: {bundle: "cn-fe-ads", ver: "1.0.0", path: name},
+					metadata:{},
+					filename: name,
+					content_type: contentType }, function(err, fileInfo) {
+						if(err){ 
+							console.log("hello");
+							d.reject(err);
+						}
+						console.dir(fileInfo);
+						d.resolve(fileInfo);
+					});
+			}else{
+				d.resolve();
+			}
 
+			return d.promise;
 
+		});
+
+        Q.allSettled(promises).then(function(){
+        	console.log("all done closing connection");
+        	db.close();
+        });
+
+	});
+
+		// db.close();
 
 });
