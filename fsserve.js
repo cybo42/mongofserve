765dd283cd63a059d8a2a66d0ef848f494cb1151
Joe Cyboski <cybo42@gmail.com>
2014-06-05 14:00:38 -0400

    Cleaned up connection creation and routing issues

--- fsserve.js
+++ fsserve.js
@@ -1,29 +1,32 @@
-var express = require('express');
+var 
+  mongo = require('mongodb')
+  , MongoClient = mongo.MongoClient
+  , Grid = mongo.Grid
+  , express = require('express')
+  , app = express()
+;
 
-
-var mongo = require('mongodb'), 
-MongoClient = mongo.MongoClient,
-Grid = mongo.Grid;
-
-
-
-var app = express();
 app.use(app.router);
 
-app.get('/s/:item(*)', function(req, res){
-  console.log("inside route");
-  console.dir(req.params);
+app.get('/debug', function(req, res){
+  res.send({routes: app.routes});
+});
 
-  var id = {
-    "bundle": "cn-fe-ads",
-    "ver": "1.0.0",
-    // "path": "image_thumb6.png"
-    "path": req.params.item
-  };
-  MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(err) throw err;
-    var grid = new Grid(db, 'fs');
-    var collection = db.collection('fs.files');
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db){
+  if(err) throw err;
+
+  var grid = new Grid(db, 'fs')
+    ,  collection = db.collection('fs.files')
+  ;
+
+  app.get('/:item(*)', function(req, res){
+    var id = {
+      "bundle": "cn-fe-ads",
+      "ver": "1.0.0",
+      "path": req.params.item
+    };
+
+    console.log("loading resource %j", id);
     collection.findOne({_id: id}, function(err, fileInfo){
       if(err){
         throw err;
@@ -37,23 +40,16 @@ app.get('/s/:item(*)', function(req, res){
         if(err){
           throw err;
         }
-        // console.log("Retrieved data: " + data.toString());
-                  res.set("Content-Type", fileInfo.contentType);
-        res.send(data);
-        // fs.writeFile('./test.png', data, function(ferr){
-          // console.log(ferr);
-          // console.log('done');
-        // });
-      });
 
+        console.log("Content-type = " + fileInfo.contentType);
+        res.set("Content-Type", fileInfo.contentType);
+        res.send(data);
+
+      });
     });
   });
-
-});
-app.get('/debug', function(req, res){
-  res.send({routes: app.routes});
 });
 
 app.listen(2000, function(){
-  console.log("online");
-})
+  console.log("online port 2000");
+});
